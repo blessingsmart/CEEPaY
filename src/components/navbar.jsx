5278606ddb165b1153b80a746660095a4349f262
Blessing Eno <bublessingsmart@gmail.com>
2024-02-09 17:04:01 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { IoReorderThree } from "react-icons/io5";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Navbar = () => {
   return (
@@ -10,7 +12,24 @@ const Navbar = () => {
                 <p className='text-sm font-semibold py-0'>Call to order</p>
                 <p className='font-bold text-xl'>0808 104 5656</p>
             </div>
-
+        </div>
+        <div className='flex h-10 bg-gray-200 justify-center items-center gap-10 font-medium'>
+            <p>CEFPaY</p>
+            <p>Become a vendor</p>
+            <p className='flex gap-1 items-center'>services
+            <IoIosArrowDown />
+            </p>
+        </div>
+        <div className='flex h-16 p-3 justify-around'>
+            <IoReorderThree size={40}/>
+            <p className='font-semibold text-3xl'>CEFPaY</p>
+            <div className='flex gap-3'>
+                <input 
+                type="text"
+                placeholder='Search' 
+                className='border-2 border-gray-200'/>
+                <button className='bg-purple-800 rounded-lg px-3'>Search</button>
+            </div>
         </div>
     </div>
   )
