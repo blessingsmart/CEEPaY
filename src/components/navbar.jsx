4ca1b279723ab17f81f6498495beadb1fe1333e2
Blessing Eno <bublessingsmart@gmail.com>
2024-02-09 06:46:03 +0100

    modified:   index.html 	modified:   src/components/navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,10 +5,10 @@ const Navbar = () => {
     <div className='flex flex-col w-screen'> 
         <div className='flex h-10'> 
             <div className='bg-black w-full'></div>
-            <div className='w-64 bg-gradient-to-r  from-purple-400 to-purple-600 border-l-4 border-white'></div>
-            <div className='flex flex-col w-52 justify-center items-center font-semibold text-sm'>
-                <h1 className=''>Call to order</h1>
-                <h1>0808 104 5656</h1>
+            <div className='w-52 bg-gradient-to-r  from-purple-400 to-purple-600 border-l-4 border-white'></div>
+            <div className='flex flex-col w-72 bg-blue-300 justify-center items-center font-semibold'>
+                <p className='text-sm font-semibold py-0'>Call to order</p>
+                <p className='font-bold text-xl'>0808 104 5656</p>
             </div>
 
         </div>
