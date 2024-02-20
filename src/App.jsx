c56c8424364efd444e522cef5ce5facac4ba2ba3
Blessing Eno <bublessingsmart@gmail.com>
2024-02-20 16:23:57 +0100

    modified:   src/App.jsx 	new file:   src/components/loading.jsx 	new file:   src/routes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import './App.css'
 import {React} from "react";
 import  {RouterProvider}  from "react-router-dom";
+import BrowserRouter from "./routes";
 
 
 function App() {
---
+++ src/routes.jsx
@@ -0,0 +1,27 @@
+import React, { lazy } from "react";
+import Loading from "../src/components/loading";
+import { createBrowserRouter }  from "react-router-dom";
+
+const Home = lazy(() => import("./pages/home"));
+// const Login = lazy(() => import("./pages/Login"));
+
+const BrowserRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <React.Suspense fallback={<Loading />}>
+                <Home />
+            </React.Suspense>
+        ),
+    },
+    // {
+    //     path: "/login",
+    //     element: (
+    //         <React.Suspense fallback={<Loading />}>
+    //             <Login />
+    //         </React.Suspense>
+    //     ),
+    // },
+]);
+
+export default BrowserRouter;
