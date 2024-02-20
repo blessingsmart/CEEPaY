d5215b946e4948b29b157b52e8e27c5714d6245d
Blessing Eno <bublessingsmart@gmail.com>
2024-02-20 15:50:05 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 // import './App.css'
-import  React from "react";
+import {React} from "react";
 import  {RouterProvider}  from "react-router-dom";
-// import BrowserRouter from "./routes";
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
